2e9792f326cf98d32dbd345e28a545e35cb8a141
Pumpkicks <154643665+flixytss@users.noreply.github.com>
2026-03-06 17:46:16 -0400

    Refactor image command to use swww-setter
    Replaced file writing and subprocess call with os.system to set wallpaper.

--- plugin.py
+++ plugin.py
@@ -6,12 +6,8 @@ class ImageCommandExtension(GObject.GObject, Nautilus.MenuProvider):
 
     def run_command(self, menu, file):
         path = file.get_location().get_path()
-        
-        with open(f"{os.getenv("HOME")}/.local/state/initback.sh", "w") as file:
-            file.write(f"#!/bin/bash\nswww img \"{path}\"")
-            file.close()
 
-        subprocess.Popen(["swww", "img", path])
+        os.system(f"swww-setter {path}") # Needs swww-setter
 
     def get_file_items(self, files):
         if len(files) != 1:
@@ -22,7 +18,6 @@ class ImageCommandExtension(GObject.GObject, Nautilus.MenuProvider):
         mime_type = file.get_mime_type()
 
         if mime_type and mime_type.startswith("image/"):
-
             item = Nautilus.MenuItem(
                 name="ImageCommandExtension::RunImage",
                 label="Set SWWW Wallpaper",
